125b000369c50704d9338cf79dfb6cc211a02ee9
kero13ro <32509189+kero13ro@users.noreply.github.com>
2023-11-07 23:01:40 +0800

    feat: add skeleton and scss

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
 </template>
 
 
-<style>
+<style lang="scss">
 body {
   margin: 0;
 }
@@ -22,5 +22,30 @@ body {
   opacity: 0;
 }
 
+.skeleton {
+  background-color: #f6f7f8;
+  position: relative;
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  &::after {
+    display: block;
+    content: '';
+    height: 100%;
+    animation: placeholderSkeleton 2s infinite linear;
+    background: linear-gradient(90deg, #f6f7f8 0%, #edeef1 20%, #f6f7f8 40%, #f6f7f8 100%);
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+}
+
+@keyframes placeholderSkeleton {
+  0% {
+    background-position: -50vw 0;
+  }
+
+  100% {
+    background-position: 70vw 0;
+  }
+}
 
 </style>
--- components/LazyImg.vue
+++ components/LazyImg.vue
@@ -14,47 +14,22 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="LazyImg">
+  <div class="LazyImg skeleton">
     <transition name="fade">
-      <img v-show="isLoading" :src="placeholder" class="skeleton" />
+      <img v-show="isLoading" :src="placeholder" class="blurImg" />
     </transition>
     <div v-if="error" class="error">alt</div>
     <img :src="src" loading="lazy" fetchpriority="low" />
   </div>
 </template>
 
-<style>
+<style lang="scss">
 .LazyImg {
   position: relative;
   overflow: hidden;
 }
-.LazyImg {
-  background-color: #f6f7f8;
-  position: relative;
-  width: 100%;
-  aspect-ratio: 1 / 1;
-}
-.LazyImg::after {
-  display: block;
-  content: '';
-  height: 100%;
-  animation: placeholderSkeleton 2s infinite linear;
-  background: linear-gradient(90deg, #f6f7f8 0%, #edeef1 20%, #f6f7f8 40%, #f6f7f8 100%);
-  background-repeat: no-repeat;
-  background-size: cover;
-}
 
-@keyframes placeholderSkeleton {
-  0% {
-    background-position: -50vw 0;
-  }
-
-  100% {
-    background-position: 70vw 0;
-  }
-}
-
-.skeleton {
+.blurImg {
   position: absolute;
   width: 100%;
   height: 100%;
